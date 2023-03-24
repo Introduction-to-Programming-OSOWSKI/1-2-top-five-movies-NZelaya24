648839f15fb3cb7dac5c06d57df4555d1e9ee361
NZelaya24 <825123@mystma.org>
2023-03-24 14:30:28 -0500

    2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #WRITE YOUR CODE IN THIS FILE
-def topFiveMovies (a , b , c , d , e):
-    return a + b + c + d + e 
+def topFiveMovies ( a , b , c , d , e):
+    return a + " " + b + " " + c + " " + d + " " + e 
 
 
-topFiveMovies ("Shrek ", "Shrek 2 ", "Shrek 3 " , "Shrek 4 " , "Shrek 5 ")
-topFiveMovies ("Spider Man " , "Iron Man ", "Doctor Strange " , "The Hulk " , "Captain America ")
+print (topFiveMovies ("Shrek", "Shrek 2", "Shrek 3" , "Shrek 4" , "Shrek 5"))
+print (topFiveMovies ("Spider Man" , "Iron Man", "Doctor Strange" , "The Hulk" , "Captain America"))
